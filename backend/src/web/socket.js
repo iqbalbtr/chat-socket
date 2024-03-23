6514c2fd5c68db8d21d3db0a7d7c2b35dc914044
Iqbal Bahtiar <iqbalbahtiar045@gmail.com>
2024-03-23 18:51:30 +0700

    add: socket provider

--- backend/src/web/socket.js
+++ backend/src/web/socket.js
@@ -40,6 +40,9 @@ module.exports = (socket) => {
     console.log("User count => ", client_active.count)
    
     return {
+        on_private: (msg) => {
+            socket.to(msg.to).emit("private-message", msg);
+        },
         disconnect: () => {
             client_active = {
                 ...client_active,
@@ -58,6 +61,21 @@ module.exports = (socket) => {
             console.log(`${current_user.username} has been disconnect`);
             console.log("disconnect =>", client_active.users.map((client) => ({ username: client.username, active: client.active })));
         },
+        on_group: (name, msg) => {
+            socket.to(name).emit("group-message", msg);
+        },
+        on_logout: (value) => {
+            client_active = {
+                count: client_active.count - 1,
+                users: client_active.users.filter(client => {
+                    return(
+                        client.username !== value.username &&
+                        client.token !== value.token
+                    )
+                })
+            }
+            console.log("logout => ", client_active.users.map((client) => ({ username: client.username, active: client.active })));
+        }
     }
 }
 
